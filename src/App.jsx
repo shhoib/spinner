24b64af90cc47f65fb3e7ce5ed0fed395f82ce2b
Shuaib Salam <shuaibsalam1234@gmail.com>
2024-01-02 18:10:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import Spinner from './components/spinner'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Spinner/>
+    </div>
+  )
+}
+
+export default App
